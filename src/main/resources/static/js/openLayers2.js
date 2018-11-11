00ec3b00d2772b23b38024b9766f85d49fa9f5db
AlexanderTUO <1599961496@qq.com>
2018-11-12 00:01:14 +0800

    官网实例

--- src/main/resources/static/js/openLayers2.js
+++ src/main/resources/static/js/openLayers2.js
@@ -1,3 +1,8 @@
+// layers、target、view是地图最基本的部分，是必需的
+var view = new ol.View({
+    center: [0, 0],
+    zoom:2
+});
 var map = new ol.Map({
     target: 'map',
     layers: [
@@ -5,8 +10,16 @@ var map = new ol.Map({
             source: new ol.source.OSM()
         })
     ],
-    view: new ol.View({
-        center: ol.proj.fromLonLat([37.41, 8.82]),
-        zoom: 4
-    })
+    loadTileWhileAnimating:true,//默认false
+    view:view
+    // view: new ol.View({
+    //     center: ol.proj.fromLonLat([37.41, 8.82]),
+    //     zoom: 4
+    // })
 });
+var london = new ol.proj.fromLonLat([-0.12755, 51.507222]);//伦敦的坐标
+
+//移动到伦敦，移动时是有动画的
+view.animate({
+    center:london
+})
