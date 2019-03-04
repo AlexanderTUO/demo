82e26c076fbbf6fe975fd7a678d2abb11f90ab2d
AlexanderTUO <42833836+AlexanderTUO@users.noreply.github.com>
2019-03-04 16:57:41 +0800

    user类

--- src/main/java/com/example/blog/model/User.java
+++ src/main/java/com/example/blog/model/User.java
@@ -1,5 +1,6 @@
 package com.example.blog.model;
 
+import com.example.blog.entity.XZQ;
 import lombok.Data;
 
 @Data
@@ -7,4 +8,22 @@ public class User {
     private Integer id;
     private String name;
     private String password;
+
+    private XZQ xzq;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String password, XZQ xzq) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.xzq = xzq;
+    }
+
+    public User(Integer id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
 }
